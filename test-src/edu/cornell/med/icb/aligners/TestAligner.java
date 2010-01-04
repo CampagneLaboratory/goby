90f551357b95fa469ca11489abee3785b07e7836
Marko Srdanovic <marko.srdanovic@gmail.com>
2010-01-04 19:58:40 +0000

    Aligner tests
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@21754 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ test-src/edu/cornell/med/icb/aligners/TestAligner.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2009 Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
+ *
+ * WEILL MEDICAL COLLEGE OF CORNELL UNIVERSITY MAKES NO REPRESENTATIONS
+ * ABOUT THE SUITABILITY OF THIS SOFTWARE FOR ANY PURPOSE. IT IS PROVIDED
+ * "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY. THE WEILL MEDICAL COLLEGE
+ * OF CORNELL UNIVERSITY SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY
+ * THE USERS OF THIS SOFTWARE.
+ */
+
+package edu.cornell.med.icb.aligners;
+
+import edu.cornell.med.icb.config.ConfigHelper;
+import edu.cornell.med.icb.goby.fastxReaders.FastXEntry;
+import edu.cornell.med.icb.goby.fastxReaders.FastXReader;
+import edu.cornell.med.icb.reads.ReadsWriter;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Fabien Campagne
+ *         Date: Jul 9, 2009
+ *         Time: 3:54:42 PM
+ */
+public class TestAligner {
+    private static final Log LOG = LogFactory.getLog(TestAligner.class);
+    private static final String BASE_TEST_DIR = "test-results/aligners";
+    private static final String[] reads = {
+            "ACTGCGCGCG",
+            "AAAAATTTTGGGGGCCCCCCC",
+            "AAAAATTTTGGGGGCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
+    };
+    final String[] references = {
+            "AGTGCGCGCG",
+            "AAAAATTTTGGGGGCCCCCCC",
+            "AAAAATTTTGGGGGCCTTCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
+    };
+
+    @Test
+    public void testAlignWithLastag() throws IOException, InterruptedException {
+        final LastagAligner aligner = new LastagAligner();
+        final String databaseDirectory = FilenameUtils.concat(BASE_TEST_DIR, "db-lastag");
+        FileUtils.forceMkdir(new File(databaseDirectory));
+        aligner.setProperties(ConfigHelper.loadConfiguration());
+
+        aligner.setDatabaseDirectory(databaseDirectory);
+        aligner.setWorkDirectory(databaseDirectory);
+
+        final File compactReads =
+                new File(FilenameUtils.concat(BASE_TEST_DIR, "lastag-input-reads.compact-reads"));
+        final File compactReference =
+                new File(FilenameUtils.concat(BASE_TEST_DIR, "lastag-input-reference.compact-reads"));
+
+        writeCompact(compactReads, reads);
+        writeCompact(compactReference, reads); // SAME AS READS!
+
+        final File fastaFile = aligner.prepareReads(compactReads);
+        assertNotNull("Reads fasta file should not be null", fastaFile);
+        assertTrue("Reads fasta file should exist", fastaFile.exists());
+
+        int readCount = 0;
+        FastXReader fastaReader = null;
+        try {
+            fastaReader = new FastXReader(fastaFile.getAbsolutePath());
+            assertEquals("File should be in fasta format", "fa", fastaReader.getFileType());
+            for (final FastXEntry entry : fastaReader) {
+                assertEquals("Mismatch for entry " + readCount,
+                        reads[readCount], entry.getSequence().toString());
+                readCount++;
+            }
+        } finally {
+            if (fastaReader != null) {
+                fastaReader.close();
+            }
+        }
+        assertEquals("Number of sequences in fasta file do not match", reads.length, readCount);
+
+        aligner.setDatabaseName(aligner.getDefaultDbNameForReferenceFile(compactReference));
+        aligner.align(compactReference, compactReads, FilenameUtils.concat(BASE_TEST_DIR, "lastag-output"));
+    }
+
+    @Test
+    public void testAlignWithBWA() throws IOException, InterruptedException {
+        final BWAAligner aligner = new BWAAligner();
+        final String databaseDirectory = FilenameUtils.concat(BASE_TEST_DIR, "db-bwa");
+        FileUtils.forceMkdir(new File(databaseDirectory));
+        aligner.setProperties(ConfigHelper.loadConfiguration());
+
+        aligner.setDatabaseDirectory(databaseDirectory);
+        aligner.setWorkDirectory(databaseDirectory);
+
+        final File compactReads =
+                new File(FilenameUtils.concat(BASE_TEST_DIR, "bwa-input-reads.compact-reads"));
+        final File compactReference =
+                new File(FilenameUtils.concat(BASE_TEST_DIR, "bwa-input-reference.compact-reads"));
+
+        writeCompact(compactReads, reads);
+        writeCompact(compactReference, references);  // Different from reads
+
+        aligner.setDatabaseName(aligner.getDefaultDbNameForReferenceFile(compactReference));
+        aligner.align(compactReference, compactReads, FilenameUtils.concat(BASE_TEST_DIR, "bwa-output"));
+    }
+
+    private void writeCompact(final File compactFile, final String[] sequences) throws IOException {
+        int index = 1;
+
+        ReadsWriter writer = null;
+        OutputStream compactFileStream = null;
+        try {
+            compactFileStream = FileUtils.openOutputStream(compactFile);
+            writer = new ReadsWriter(compactFileStream);
+            writer.setNumEntriesPerChunk(9);
+
+            for (final String sequence : sequences) {
+                writer.setSequence(sequence);
+                writer.setIdentifier(Integer.toString(index++));
+                writer.appendEntry();
+            }
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+            IOUtils.closeQuietly(compactFileStream);
+        }
+    }
+
+    @BeforeClass
+    public static void initializeTestDirectory() throws IOException {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Creating base test directory: " + BASE_TEST_DIR);
+        }
+
+        FileUtils.forceMkdir(new File(BASE_TEST_DIR));
+    }
+
+    @AfterClass
+    public static void cleanupTestDirectory() throws IOException {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Deleting base test directory: " + BASE_TEST_DIR);
+        }
+        FileUtils.forceDeleteOnExit(new File(BASE_TEST_DIR));
+    }
+}
