b24dd0a5a9b64c9f3349c165e7ee3e9324303b69
Kevin C. Dorf <kdorff@campagnelab.org>
2010-01-07 20:10:58 +0000

    When no output is specified, Compact to Counts writes output to basename. I've made Counts to Wiggles also write to basename when no output is specified. More convienient this way.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@21830 60cd2ccc-2b05-0410-9566-da273a940bff

--- src/edu/cornell/med/icb/goby/modes/CountsArchiveToWiggleMode.java
+++ src/edu/cornell/med/icb/goby/modes/CountsArchiveToWiggleMode.java
@@ -1,19 +1,12 @@
 /*
- * Copyright (C) 2009-2010 Institute for Computational Biomedicine,
- *                         Weill Medical College of Cornell University
+ * Copyright (C) 2009 Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * WEILL MEDICAL COLLEGE OF CORNELL UNIVERSITY MAKES NO REPRESENTATIONS
+ * ABOUT THE SUITABILITY OF THIS SOFTWARE FOR ANY PURPOSE. IT IS PROVIDED
+ * "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY. THE WEILL MEDICAL COLLEGE
+ * OF CORNELL UNIVERSITY SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY
+ * THE USERS OF THIS SOFTWARE.
  */
 
 package edu.cornell.med.icb.goby.modes;
@@ -72,12 +65,10 @@ public class CountsArchiveToWiggleMode extends AbstractGobyMode {
     private String alternativeCountArchiveExtension;
     private int resolution;
 
-    @Override
     public String getModeName() {
         return MODE_NAME;
     }
 
-    @Override
     public String getModeDescription() {
         return MODE_DESCRIPTION;
     }
@@ -116,8 +107,8 @@ public class CountsArchiveToWiggleMode extends AbstractGobyMode {
             filterByReferenceNames = true;
         }
         if (outputFile == null) {
-            outputFile = FilenameUtils.getBaseName(inputBasename) + ".wiggle" + (includeReferenceNameComas != null ? ("-" + includeReferenceNameComas) :
-                    "-all");
+            outputFile = inputBasename + ".wiggle" +
+                    (includeReferenceNameComas != null ? ("-" + includeReferenceNameComas) : "-all");
         }
 
         return this;
