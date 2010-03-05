4c4c147eae835c111b69f296013d06633a29a017
Marko Srdanovic <marko.srdanovic@gmail.com>
2010-03-05 18:08:01 +0000

    Two template examples - one for "statndard" alignments and the other for transcripts.  Should be combined at some point.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@22436 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ scripts/sge/sge-transcript-env-template.sh
@@ -0,0 +1,56 @@
+#
+# Environment variables used for SGE alignment jobs
+#
+
+#
+# Email address to notify upon job completion
+#
+SGE_STATUS_MAILTO=
+
+#
+# Memory size for SGE
+#
+SGE_MEMORY=4g
+
+#
+# JVM flags (memory should match SGE_MEMORY)
+#
+SGE_JVM_FLAGS=-Xmx4g
+
+#
+# Aligner
+#
+ALIGNER=bwa
+
+#
+# Full path to native aligner executable directory
+#
+BWA_ALIGNER_PATH=
+LAST_ALIGNER_PATH=
+LASTAG_ALIGNER_PATH=
+
+#
+# Colorspace option (comment out or leave blank if not using colorspace)
+#
+#COLORSPACE=--color-space
+COLORSPACE=
+
+#
+# Full path to the reads file
+#
+READS=
+
+#
+# Full path to directory containing one or more transcripts in compact format
+#
+TRANSCRIPT_DIRECTORY=
+
+#
+# Full path to the index directory (i.e., reference cache) for transcripts
+#
+TRANSCRIPT_INDEX_DIRECTORY=
+
+#
+# Full path to gene to transcript mapping file
+#
+GENE_TRANSCRIPT_MAP_FILE=
