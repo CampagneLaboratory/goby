4479e720d88f0a6c38c1622f812e484923982ec7
Marko Srdanovic <marko.srdanovic@gmail.com>
2010-03-19 18:21:05 +0000

    description/comments for the data stored in the aligned sequence object.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@22618 60cd2ccc-2b05-0410-9566-da273a940bff

--- src/edu/cornell/med/icb/goby/alignments/AlignedSequence.java
+++ src/edu/cornell/med/icb/goby/alignments/AlignedSequence.java
@@ -28,16 +28,55 @@ import it.unimi.dsi.lang.MutableString;
  *         Time: 3:54:17 PM
  */
 public class AlignedSequence {
+    /**
+     * The name of one of the source sequences for the alignment.
+     */
+    public final MutableString sequenceIdentifier;
+
+    /**
+     * The start of the aligning region in the source sequence. This is a zero-based number.
+     * If the @link #strand} is '-' then this is the start relative to the reverse-complemented
+     * source sequence.
+     */
+    public int alignedStart;
+
+    /**
+     * The start of the aligning region in the source sequence. This is a zero-based number.
+     * If the {@link #strand} is '-' then this is the start relative to the
+     * reverse-complemented source sequence.
+     */
+    public int alignedLength;
+
+    /**
+     * Either '+' or '-'. If '-', then the alignment is to the reverse-complemented source.
+     */
+    public char strand;
+
+    /**
+     * The size of the entire source sequence, not just the parts involved in the alignment.
+     */
+    public int sequenceLength;
+
+    /**
+     * The nucleotides (or amino acids) in the alignment and any insertions (dashes) as well.
+     */
+    public final MutableString alignment;
+
+    /**
+     * Create a new aligned sequence object.
+     * Typically contents of this object maps to Lines starting with 's'
+     * (a sequence within an alignment block) in MAF alignment file as in the following example:
+     *
+     * <pre>
+     * s hg16.chr7    27707221 13 + 158545518 gcagctgaaaaca
+     * s panTro1.chr6 28869787 13 + 161576975 gcagctgaaaaca
+     * s baboon         249182 13 +   4622798 gcagctgaaaaca
+     * s mm4.chr6     53310102 13 + 151104725 ACAGCTGAAAATA
+     * </pre>
+     */
     public AlignedSequence() {
+        super();
         sequenceIdentifier = new MutableString();
         alignment = new MutableString();
     }
-
-    public MutableString sequenceIdentifier;
-    public int alignedStart;
-    public int alignedLength;
-    public char strand;
-    public int sequenceLength;
-    public MutableString alignment;
-
 }
