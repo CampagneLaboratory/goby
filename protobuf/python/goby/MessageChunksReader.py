9e1b352040ec9823cc62e66739461c5e3b2055ab
Marko Srdanovic <marko.srdanovic@gmail.com>
2010-05-11 23:40:21 +0000

    Rough cut at python implementation of MessageChunksReader class.  Can read chunk boundaries/lengths properly and now need to get the data into protobuf, etc.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@23243 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ protobuf/python/goby/MessageChunksReader.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+#
+# Copyright (C) 2010 Institute for Computational Biomedicine,
+#                    Weill Medical College of Cornell University
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import getopt
+import os
+import struct
+import sys
+
+def read_utf(fd):
+    "Python implementation of Java's DataInputStream.readUTF method."
+    length = struct.unpack('>H', fd.read(2))[0]
+    return fd.read(length).decode('utf8')
+
+def read_int(fd):
+    "Python implementation of Java's DataInputStream.readInt method."
+    return struct.unpack('>I', fd.read(4))[0]
+
+def usage():
+    print "usage:", sys.argv[0], "[-h|--help] [-v|--verbose] <basename>" 
+
+def main():
+    DELIMITER_CONTENT = 0xFF;
+    DELIMITER_LENGTH = 8;
+
+    verbose = False
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "verbose"])
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(1)
+
+    # Collect options
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-v", "--verbose"):
+            verbose = True
+        
+    if len(args) != 1:
+        usage()
+        sys.exit(2)
+
+    basename = args[0]
+    print "Compact Alignment basename =", basename
+
+    # read the entries file
+    entries_filename = basename + ".entries"
+    if verbose:
+        print "Reading entries from", entries_filename
+    f = open(entries_filename, "rb")
+    try:
+        num_bytes = None
+        while num_bytes != 0:
+            f.seek(DELIMITER_LENGTH, os.SEEK_CUR)
+            num_bytes = read_int(f)
+            print num_bytes
+            if (num_bytes != 0):
+                f.seek(num_bytes, os.SEEK_CUR)
+        
+    finally:
+        f.close()
+
+if __name__ == "__main__":
+    main()
+        
