9d6c7dac67f9e8e5de464b5d55d6ce17ab24053f
Marko Srdanovic <marko.srdanovic@gmail.com>
2010-06-04 00:07:46 +0000

    C++ implementation of Goby TooManyHits
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@23648 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ cpp/src/goby/TooManyHits.cc
@@ -0,0 +1,142 @@
+/**
+ * Copyright (C) 2010 Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include "Alignments.pb.h"
+#include "TooManyHits.h"
+
+using namespace std;
+
+namespace goby {
+  /*
+   * TooManyHits
+   */
+  TooManyHits::TooManyHits(string basename) {
+    // store the basename
+    this->basename = basename;
+    this->pbTmh = AlignmentTooManyHits::default_instance();
+  }
+
+  int TooManyHits::getAlignerThreshold() const {
+    return pbTmh.alignerthreshold();
+  }
+  
+  vector<int> TooManyHits::getQueryIndicies() const {
+    // copy the keys from the queryIndex2NumHits and return as a new vector 
+    vector<int> queryIndicies(queryIndex2NumHits.size());
+    int i = 0;
+    for (map<int, int>::const_iterator it = queryIndex2NumHits.begin(); it != queryIndex2NumHits.end(); it++) {
+      queryIndicies[i++] = it->first;
+    }
+
+    return queryIndicies;
+  }
+  
+  int TooManyHits::getNumberOfHits(int queryIndex) const {
+    int numberOfHits;
+    map<int, int>::const_iterator it = queryIndex2NumHits.find(queryIndex);
+    if (it != queryIndex2NumHits.end()) {
+      numberOfHits = it->second;
+    } else {
+      numberOfHits = -1;
+    }
+    return numberOfHits;
+  }
+
+  int TooManyHits::getLengthOfMatch(int queryIndex) const {
+    int lengthOfMatch;
+    map<int, int>::const_iterator it = queryIndex2Depth.find(queryIndex);
+    if (it != queryIndex2NumHits.end()) {
+      lengthOfMatch = it->second;
+    } else {
+      lengthOfMatch = -1;
+    }
+    return lengthOfMatch;
+  }
+  
+  bool TooManyHits::isQueryAmbiguous(int queryIndex) const {
+    // TODO
+  }
+
+  bool TooManyHits::isQueryAmbiguous(int queryIndex, int k) const {
+    // TODO
+  }
+
+  bool TooManyHits::isQueryAmbiguous(int queryIndex, int k, int matchLength) const {
+    // TODO
+  }
+
+  ostream& operator<<(ostream& out, const TooManyHits& tmh) {
+    tmh.pbTmh.SerializeToOstream(&out);
+    return out;
+  }
+
+  string& operator<<(string& out, const TooManyHits& tmh) {
+    tmh.pbTmh.SerializeToString(&out);
+    return out;
+  }
+
+  TooManyHits& operator<<(TooManyHits& tmh, istream& in) {
+    tmh.pbTmh.ParseFromIstream(&in);
+    return tmh;
+  }
+
+  TooManyHits& operator<<(TooManyHits& tmh, string& in) {
+    tmh.pbTmh.ParseFromString(in);
+    return tmh;
+  }
+
+  TooManyHits::~TooManyHits(void) {
+  }
+
+  /*
+   * TooManyHitsReader
+   */
+  TooManyHitsReader::TooManyHitsReader(string basename) : TooManyHits(basename) {
+    // open the "tmh" file
+    string tmhFilename = basename + ".tmh";
+    ifstream tmhStream(tmhFilename.c_str(), ios::in | ios::binary);
+
+    // populate the too many hits object from the file
+    if (!pbTmh.ParseFromIstream(&tmhStream)) {
+      cerr << "Failed to parse too many hits file" << endl;
+    }
+
+    tmhStream.close();
+
+    // populate the query index to number of hits and depth maps
+    google::protobuf::RepeatedPtrField<const goby::AmbiguousLocation>::const_iterator hitsIterator;
+    for (hitsIterator = pbTmh.hits().begin(); hitsIterator != pbTmh.hits().end(); hitsIterator++) {
+      int queryIndex = hitsIterator->query_index();
+      queryIndex2NumHits[queryIndex] = hitsIterator->at_least_number_of_hits();
+      if (hitsIterator->has_length_of_match()) {
+	  queryIndex2Depth[queryIndex] = hitsIterator->length_of_match();
+      }
+    }
+
+    cout << "Number of hits = " << pbTmh.hits().size() << endl;
+    cout << "Number of query entries = " << queryIndex2NumHits.size() << endl;
+  }
+
+  TooManyHitsReader::~TooManyHitsReader(void) {
+  }
+}
---
+++ cpp/src/goby/TooManyHits.h
@@ -0,0 +1,87 @@
+/**
+ * Copyright (C) 2010 Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifndef GOBY_TOO_MANY_HITS_H
+#define GOBY_TOO_MANY_HITS_H
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include "Alignments.pb.h"
+
+namespace goby {
+  class TooManyHits {
+    std::string basename;
+
+  protected:
+    AlignmentTooManyHits pbTmh;
+
+    // A map from query index to the number of hits at that index.
+    std::map<int, int> queryIndex2NumHits;
+
+    // A map from query index to depth/length of match.
+    std::map<int, int> queryIndex2Depth;
+
+  public:
+    TooManyHits(std::string basename);
+    virtual ~TooManyHits(void);
+
+    //TooManyHits(const TooManyHits& from);
+    //TooManyHits& operator=(const TooManyHits& from);
+
+    inline std::string getBasename() const { return basename; };
+
+    // Number of hits that the aligner considered was too many to report.
+    int getAlignerThreshold() const;
+    std::vector<int> getQueryIndicies() const;
+
+    int getNumberOfHits(int queryIndex) const;
+    int getLengthOfMatch(int queryIndex) const;
+
+    bool isQueryAmbiguous(int queryIndex) const;
+    bool isQueryAmbiguous(int queryIndex, int k) const;
+    bool isQueryAmbiguous(int queryIndex, int k, int matchLength) const;
+
+    friend std::ostream &operator<<(std::ostream &out, const TooManyHits& tmh);
+    friend std::string& operator<<(std::string& out, const TooManyHits& tmh);
+    friend TooManyHits& operator<<(TooManyHits& tmh, std::istream& in);
+    friend TooManyHits& operator<<(TooManyHits& tmh, std::string& in);
+  };
+
+  class TooManyHitsReader : public TooManyHits {
+  public:
+    TooManyHitsReader(std::string basename);
+    ~TooManyHitsReader(void);
+  };
+
+  class TooManyHitsWriter : public TooManyHits {
+  public:
+    TooManyHitsWriter(std::string basename);
+    ~TooManyHitsWriter(void);
+
+    inline void setAlignerThreshold(int threshold) { pbTmh.set_alignerthreshold(threshold); };
+
+    void write();
+  };
+
+}
+
+#endif // GOBY_TOO_MANY_HITS_H
