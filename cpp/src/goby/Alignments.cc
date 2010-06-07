258aba4324e6a17a979d7b6bca3ec39417fbeb52
Marko Srdanovic <marko.srdanovic@gmail.com>
2010-06-07 23:01:22 +0000

    Utility methods for basename retrieval
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@23735 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ cpp/src/goby/Alignments.cc
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2010 Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include "Alignments.h"
+
+using namespace std;
+
+namespace goby {
+  Alignments::Alignments(string basename) {
+    this->basename = basename;
+  }
+
+  Alignments::~Alignments(void) {
+  }
+
+  string Alignments::getBasename(const char* filename) {
+   return getBasename(string(filename));
+  }
+
+  string Alignments::getBasename(const string& filename) {
+    const string COMPACT_ALIGNMENT_FILE_EXTS[] = {
+      ".entries", ".header", ".tmh", ".stats", ".counts"
+    };
+
+    const size_t len = sizeof(COMPACT_ALIGNMENT_FILE_EXTS) / sizeof(COMPACT_ALIGNMENT_FILE_EXTS[0]);
+    const size_t dotindex = filename.find_last_of(".");
+    if (dotindex != string::npos) {
+      const string extension = filename.substr(dotindex);
+      for (int i = 0; i < len; i++) {
+        if (extension.compare(COMPACT_ALIGNMENT_FILE_EXTS[i]) == 0) {
+          return filename.substr(0, dotindex);
+        }
+      }
+    }
+
+    return filename;
+  }
+}
