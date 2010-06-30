dbc45e8a38293708955caf6ddb17fa01bb81edb5
Marko Srdanovic <marko.srdanovic@gmail.com>
2010-06-30 22:58:56 +0000

    Re-implement the MessageChunksIterator so that it doesn't pre-read all the chunk positions on initialization.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@24119 60cd2ccc-2b05-0410-9566-da273a940bff

--- cpp/src/goby/MessageChunks.h
+++ cpp/src/goby/MessageChunks.h
@@ -23,11 +23,12 @@
 
 #include <fstream>
 #include <iostream>
-#include <istream>
 #include <iterator>
 #include <string>
 #include <vector>
+
 #include <google/protobuf/io/gzip_stream.h>
+#include <google/protobuf/io/zero_copy_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
 #include "common.h"
@@ -36,69 +37,75 @@
 namespace goby {
   #define GOBY_MESSAGE_CHUNK_DELIMITER_LENGTH 8      // length of the delimiter tag (in bytes)
 
-  // details of an individual chunk of protocol buffer messages
-  struct MessageChunkInfo {
-    // the position within the stream
-    std::streampos position;
-    // the length of the chunk
-    size_t length;
-  };
-
   template <typename T> class MessageChunksIterator : public std::iterator<std::input_iterator_tag, T> {
     // the name of the chunked file
     std::string filename;
 
-    // positions of compressed alignment collections within the goby chunked file
-    std::vector<MessageChunkInfo> chunks;
+    // The underlying stream
+    std::ifstream stream;
 
-    // index to the current chunk details
-    int chunkIndex;
+    // The current position in the stream (we keep this since "tellg()" is not const)
+    std::streampos current_position;
 
     // the current processed chunk
-    T currentChunk;
+    T current_chunk;
 
     // populate T with the data from the given chunk
-    T populateChunk(T& chunk, const MessageChunkInfo& chunkInfo) {
-       std::ifstream stream(filename.c_str(), std::ios::in | std::ios::binary);
-
-      // position the stream to the current chunk location
-      stream.seekg(chunkInfo.position, std::ios::beg);
-
-      // Set up a stream that will only read up the current chunk length
-      const size_t compressedChunkLength = chunkInfo.length;
-      google::protobuf::io::IstreamInputStream istream(&stream);
-      google::protobuf::io::LimitingInputStream rawChunkStream(&istream, compressedChunkLength);
-
-      // uncompress the buffer so that it can be parsed
-      google::protobuf::io::GzipInputStream chunkStream(&rawChunkStream);
-
-      // the stream may not get all read in at once so we may need to copy in stages
-      void* uncompressedChunk = NULL;
-      size_t chunkSize = 0;
-
-      const void* buffer;
-      int bufferSize;
-      while (chunkStream.Next(&buffer, &bufferSize)) {
-        // store the end location of the chunk buffer
-        int index = chunkSize;
-
-        // resize the chunk buffer to fit the new data just read
-        chunkSize += bufferSize;
-        uncompressedChunk = (void *)realloc(uncompressedChunk, chunkSize);
-
-        // and append the new data over to the end of the header buffer
-        ::memcpy(reinterpret_cast<char*>(uncompressedChunk) + index, buffer, bufferSize);
+    // The assumption here is that the stream is positioned at the start of a chunk boundary
+    // The stream will be positioned at the next chunk boundary or eof
+    T populateChunk(std::ifstream& stream, T& chunk) {
+      if (!stream.good()) {
+        current_position = static_cast<std::streampos>(-1);
+        chunk.Clear();
+        return chunk;
       }
 
-      // populate the current object from the uncompressed data
-      if (!chunk.ParseFromArray(uncompressedChunk, chunkSize)) {
+      // each chunk is delimited by DELIMITER_LENGTH bytes
+      stream.seekg(GOBY_MESSAGE_CHUNK_DELIMITER_LENGTH, std::ios::cur);
+      current_position = stream.tellg();
+
+      // Set up a stream that will only read up the current chunk length
+      const size_t chunkLength = readInt(stream);
+      current_position = stream.tellg();
+
+#ifdef GOBY_DEBUG
+      std::cout << "Chunk length: " << chunkLength << std::endl;
+#endif
+
+      // the last chunk has a length of zero bytes
+      if (stream.eof() || chunkLength == 0) {
+        current_position = static_cast<std::streampos>(-1);
+        chunk.Clear();
+        return chunk;
+      }
+
+      const std::streampos chunkStartPosition = current_position;
+      const std::streampos nextChunkStartPosition = current_position + static_cast<std::streamoff>(chunkLength);
+
+      // explicitly set the block size and limit it to the chunk length to prevent "over read"
+      google::protobuf::io::IstreamInputStream istream(&stream, chunkLength);
+      google::protobuf::io::LimitingInputStream rawChunkStream(&istream, chunkLength);
+
+      // and handle the fact that each chunk is compressed with gzip
+      google::protobuf::io::GzipInputStream chunkStream(&rawChunkStream);
+
+      // populate the current object from the compressed data
+      if (!chunk.ParseFromZeroCopyStream(&chunkStream)) {
         std::cerr << "Failed to parse message chunk from " << filename << std::endl;
       }
 
-      // free up the temporary buffers and close the stream
-      ::free(uncompressedChunk);
+      current_position = stream.tellg();
 
-      stream.close();
+      // may need to adjust the position in the stream since the parsing above may have "over-read"
+      // TODO - can probably remove this block since the block size is set above
+      if (current_position != static_cast<std::streampos>(-1)) {
+        const std::streamoff offset = nextChunkStartPosition - current_position;
+        if (offset != 0) {
+          stream.seekg(offset, std::ios::cur);
+          current_position = stream.tellg();
+          std::cerr << "WARNING: Stream position adjuted to: " << current_position << std::endl;
+        }
+      }
 
       // and retrun the processed chunk
       return chunk;
@@ -115,115 +122,93 @@ namespace goby {
     };
 
   public:
-    // TODO: MessageChunksIterator(int fileDescriptor);
-    // TODO: MessageChunksIterator(istream* stream);
-    MessageChunksIterator(const std::string& filename) : filename(filename) {
-      chunks.clear();
+    // TOOD: currently assumes that the position is at the start of chunk boundary
+    MessageChunksIterator<T>(const std::string& filename,  const std::streampos position = 0) :
+        filename(filename),
+        current_position(position),
+        current_chunk(T::default_instance()) {
+      stream.open(filename.c_str(), std::ios::in | std::ios::binary);
+      stream.seekg(position);
 
-      std::ifstream stream(filename.c_str(), std::ios::in | std::ios::binary);
-
-      int chunkNumber = 0;
-      // get the positions each of the chunks in the file
-      while (stream.good()) {
-        // each chunk is delimited by DELIMITER_LENGTH bytes
-        stream.seekg(GOBY_MESSAGE_CHUNK_DELIMITER_LENGTH, std::ios::cur);
-
-        // then the size of the next chunk follows
-        const int size = readInt(stream);
-#if GOBY_DEBUG
-        std::cout << "length of chunk #" << chunkNumber++ << " is " << size << std::endl;
-#endif // GOBY_DEBUG
-
-        // the last chunk has a size of zero bytes
-        if (!stream.eof() && size != 0) {
-          const std::streampos position = stream.tellg();
-          MessageChunkInfo chunkInfo;
-          chunkInfo.position = position;
-          chunkInfo.length = size;
-          chunks.push_back(chunkInfo);
-          stream.seekg(size, std::ios::cur);
-        } else {
-          break;
-        }
+      if (stream.good()) {
+       populateChunk(stream, current_chunk);
+      } else {
+        std::cerr << "Failed to open " << filename << std::endl;
       }
-
-      stream.close();
-
-      this->chunkIndex = 0;
-      this->currentChunk = T::default_instance();
     };
 
     virtual ~MessageChunksIterator(void) {
+      stream.close();
     };
 
-    MessageChunksIterator(const MessageChunksIterator& reader)
-      : filename(reader.filename),
-        chunks(reader.chunks),
-        chunkIndex(reader.chunkIndex),
-        currentChunk(reader.currentChunk) {
+    MessageChunksIterator(const MessageChunksIterator<T>& that) :
+        filename(that.filename),
+        current_position(that.current_position),
+        current_chunk(that.current_chunk) {
+      // TODO: there is probably a better way to copy the stream
+      stream.open(filename.c_str(), std::ios::in | std::ios::binary);
+      stream.seekg(current_position);
       std::cout << "MessageChunksIterator Copy constructor" << std::endl;
     }
 
-    MessageChunksIterator(const MessageChunksIterator& reader, int chunkIndex)
-      : filename(reader.filename),
-        chunks(reader.chunks),
-        chunkIndex(chunkIndex),
-        currentChunk(reader.currentChunk) {
+    MessageChunksIterator(const MessageChunksIterator<T>& that, std::streamoff off, std::ios_base::seekdir dir = std::ios_base::beg) :
+      filename(that.filename) {
+      // TODO: there is probably a better way to copy the stream
+      stream.open(filename.c_str(), std::ios::in | std::ios::binary);
+      stream.seekg(off, dir);
+      current_position = stream.tellg();
+      populateChunk(stream, current_chunk);
       std::cout << "MessageChunksIterator Copy constructor" << std::endl;
     }
 
+    // TODO: Prefix and Postfix operators currently do the same thing!
     // Prefix increment operator
     MessageChunksIterator& operator++() {
       std::cout << "Prefix operator++() " << std::endl;
-      ++chunkIndex;
+      populateChunk(stream, current_chunk);
       return *this;
     };
 
     // Postfix increment operator
     MessageChunksIterator& operator++(int) {
       std::cout << "Postfix operator++(int) " << std::endl;
-      chunkIndex++;
+      populateChunk(stream, current_chunk);
       return *this;
     };
 
-    bool operator==(const MessageChunksIterator<T>& rhs) const {
-      // the filenames must match and either the chunks are
-      // both empty or the chunk positions are the same
-      return filename == rhs.filename && chunkIndex == rhs.chunkIndex;
+    bool operator==(MessageChunksIterator<T>& rhs) const {
+      // the filenames and the stream positions must match
+      return filename == rhs.filename && current_position == rhs.current_position;
     };
 
-    bool operator!=(const MessageChunksIterator<T>& rhs) const {
-      // the filenames must match and either the chunks are
-      // both empty or the chunk positions are the same
-      return filename != rhs.filename || chunkIndex != rhs.chunkIndex;
+    bool operator!=(MessageChunksIterator<T>& rhs) const {
+      // the filenames and the stream positions must match
+      return filename != rhs.filename || current_position != rhs.current_position;
     };
 
     // return the parsed results for the current chunk
-    // the contract of the input iterator is that this is only done once
-    // so we uncompress at this time rather than during the increment
     const T& operator*() {
-      populateChunk(currentChunk, chunks[chunkIndex]);
-      return currentChunk;
+      return current_chunk;
     };
 
     T* const operator->() {
-      populateChunk(currentChunk, chunks[chunkIndex]);
-      return &currentChunk;
+      return &current_chunk;
     };
 
     // TODO - remove the operator<< - for testing only
-    friend std::ostream &operator<<(std::ostream &out, const MessageChunksIterator& reader) {
-      out << "ostream &operator<< " << reader.chunkIndex;
+    friend std::ostream &operator<<(std::ostream &out, MessageChunksIterator& iterator) {
+      out << "ostream &operator<< " << iterator.current_position;
       return out;
     };
 
-    MessageChunksIterator begin() const {
-      return MessageChunksIterator(*this, 0);
+    MessageChunksIterator begin() {
+      return MessageChunksIterator(*this, static_cast<std::streamoff>(0), std::ios_base::beg);
     };
 
-    MessageChunksIterator end() const {
-      return MessageChunksIterator(*this, chunks.size());
+    MessageChunksIterator end() {
+      return MessageChunksIterator(*this, static_cast<std::streamoff>(0), std::ios_base::end);
     };
+
     /*
     MessageChunksIterator& operator=(const MessageChunksIterator<T>& that)  {
       if (this != &that) {
