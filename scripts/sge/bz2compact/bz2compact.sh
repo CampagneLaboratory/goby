064a7981feeaf8ab1411cd2c5f1461969fbfe9a4
Fabien Campagne <fac2003@campagnelab.org>
2010-10-15 17:29:58 +0000

    Draft version
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@25369 60cd2ccc-2b05-0410-9566-da273a940bff

--- scripts/sge/bz2compact/bz2compact.sh
+++ scripts/sge/bz2compact/bz2compact.sh
@@ -5,5 +5,13 @@
 #
 
 # Run the aligner
-BZ2COMPACT=`qsub -terse bz2compact.qsub`
-echo $BZ2COMPACT
+
+export WORKING_DIR=`dirname "$0"`
+export GOBY=`which goby`
+export BASENAME=`basename $1 .fastq.bz2`
+export DATA_DIR=`dirname $1`
+
+CMD=`qsub -terse -v GOBY=$GOBY -v BASENAME=$BASENAME -v DATA_DIR=$DATA_DIR ${WORKING_DIR}/bz2compact.qsub `
+echo $CMD
+
+
