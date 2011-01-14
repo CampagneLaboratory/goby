37c3e6c55d68b885994f3053da25c2167ca82598
Fabien Campagne <fac2003@campagnelab.org>
2011-01-14 17:07:08 +0000

    Add quality encoding to bz2compact convert script.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@26377 60cd2ccc-2b05-0410-9566-da273a940bff

--- scripts/sge/bz2compact.sh
+++ scripts/sge/bz2compact.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #
-# Script to convert a fastq.bz2 file to compact format.
+# Script to convert a fastq.bz2 file to compact format. The second argument must provide the quality encoding
+# used in the fastq file.
 # Example of use:
 # foreach file (full-path/*.fastq.bz2)
-#  bz2compact.sh $file
+#  bz2compact.sh $file (SANGER|ILLUMINA|SOLEXA)
 # end
 # The previous lines will convert each file with extension .fastq.bz2 to the compact-reads format, running
 # in parallel on an SGE grid. The file bz2compact.sh must be on the path.
@@ -14,8 +15,9 @@ export WORKING_DIR=`dirname "$0"`
 export GOBY=`which goby`
 export BASENAME=`basename $1 .fastq.bz2`
 export DATA_DIR=`dirname $1`
+export QUALITY_ENCODING=$2
 
-CMD=`qsub -terse -v GOBY=$GOBY -v BASENAME=$BASENAME -v DATA_DIR=$DATA_DIR ${WORKING_DIR}/bz2compact.qsub `
+CMD=`qsub -terse -v GOBY=$GOBY -v BASENAME=$BASENAME -v DATA_DIR=$DATA_DIR -v QUALITY_ENCODING=$QUALITY_ENCODING ${WORKING_DIR}/bz2compact.qsub `
 echo $CMD
 
 
