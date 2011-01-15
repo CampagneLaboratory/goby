87a0138ccf65cc0d0af328f23ebacba22715a724
Fabien Campagne <fac2003@campagnelab.org>
2011-01-15 18:37:08 +0000

    Suggestion for autogen on mac OS. The previous version is commented out. See http://myitcorner.com/blog/?p=207 for details about the error (Macro PKG_CHECK_MODULES is not found when running goby/cpp/autogen.sh on MAC OS).
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@26397 60cd2ccc-2b05-0410-9566-da273a940bff

--- cpp/autogen.sh
+++ cpp/autogen.sh
@@ -2,5 +2,10 @@
 
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
-autoreconf --force --install --verbose "$srcdir"
+#autoreconf --force -I /opt/local/share/aclocal --install --verbose "$srcdir"
+# The following seems to work better on MacOs than autoreconf. Needs testing on other platforms as well.
+aclocal -I /opt/local/share/aclocal
+autoheader
+automake
+autoconf
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
