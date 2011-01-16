6c170eb5203b78f4d8a4d6ba815be828aa0dbf90
Fabien Campagne <fac2003@campagnelab.org>
2011-01-16 16:15:50 +0000

    Tested autogen on both MacOS and Linux. configure now runs successfully on MacOS. A linking error still occurs, but we are getting closer to compiling goby cpp on Mac.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@26404 60cd2ccc-2b05-0410-9566-da273a940bff

--- cpp/autogen.sh
+++ cpp/autogen.sh
@@ -3,8 +3,15 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 #autoreconf --force -I /opt/local/share/aclocal --install --verbose "$srcdir"
-# The following seems to work better on MacOs than autoreconf. Needs testing on other platforms as well.
-aclocal -I /opt/local/share/aclocal
+# Need to set the following variable based on OS. MacOS needs the -I option to aclocal
+OS=`uname -s`
+if [ $OS = "Darwin" ]; then
+  INCLUDE_OPTION=-I /opt/local/share/aclocal
+ else
+  INCLUDE_OPTION=""
+fi
+
+aclocal $INCLUDE_OPTION
 autoheader
 automake
 autoconf
