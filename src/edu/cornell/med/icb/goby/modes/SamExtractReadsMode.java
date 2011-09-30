19d90accff14e94a7ec39d914283a44efe3a3beb
Fabien Campagne <fac2003@campagnelab.org>
2011-09-30 16:47:04 +0000

    Support importing paired end BAM files.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@28936 60cd2ccc-2b05-0410-9566-da273a940bff

--- src/edu/cornell/med/icb/goby/modes/SamExtractReadsMode.java
+++ src/edu/cornell/med/icb/goby/modes/SamExtractReadsMode.java
@@ -26,6 +26,7 @@ import it.unimi.dsi.lang.MutableString;
 import it.unimi.dsi.logging.ProgressLogger;
 import net.sf.samtools.SAMFileReader;
 import net.sf.samtools.SAMRecord;
+import net.sf.samtools.SAMRecordIterator;
 import org.apache.log4j.Logger;
 
 import java.io.File;
@@ -54,9 +55,9 @@ public class SamExtractReadsMode extends AbstractGobyMode {
     private static final String MODE_DESCRIPTION = "Extract reads from a SAM file.";
 
     /**
-     * The input filename.
+     * The input filenames.
      */
-    private String inputFilename;
+    private String inputFilenames[];
 
     /**
      * The output file.
@@ -75,6 +76,11 @@ public class SamExtractReadsMode extends AbstractGobyMode {
         return MODE_DESCRIPTION;
     }
 
+    private boolean processPairs = false;
+    private String pairIndicator1 = null;
+    private String pairIndicator2 = null;
+    int processAtMost = -1;
+
     /**
      * Configure.
      *
@@ -88,12 +94,26 @@ public class SamExtractReadsMode extends AbstractGobyMode {
     public AbstractCommandLineMode configure(final String[] args) throws IOException, JSAPException {
         final JSAPResult jsapResult = parseJsapArguments(args);
 
-        inputFilename = jsapResult.getString("input");
+        inputFilenames = jsapResult.getStringArray("input");
         outputFilename = jsapResult.getString("output");
         if (!outputFilename.endsWith(".compact-reads")) {
             outputFilename = outputFilename + ".compact-reads";
         }
         qualityEncoding = QualityEncoding.valueOf(jsapResult.getString("quality-encoding").toUpperCase());
+
+        processPairs = jsapResult.getBoolean("paired-end");
+        final String tokens = jsapResult.getString("pair-indicator");
+        if (processPairs && tokens != null) {
+            final String[] tmp = tokens.split("[,]");
+            if (tmp.length != 2) {
+                System.err.println("Pair indicator argument must have exactly two tokens, separated by " +
+                        "comma. Offending syntax: " + tokens);
+                System.exit(1);
+            }
+            pairIndicator1 = tmp[0];
+            pairIndicator2 = tmp[1];
+        }
+        processAtMost = jsapResult.getInt("process-at-most");
         return this;
     }
 
@@ -108,26 +128,78 @@ public class SamExtractReadsMode extends AbstractGobyMode {
     public void execute() throws IOException {
         final ReadsWriter writer = new ReadsWriter(new FileOutputStream(outputFilename));
         try {
-            final ProgressLogger progress = new ProgressLogger(LOG);
-            // the following is required to set validation to SILENT before loading the header (done in the SAMFileReader constructor)
-            SAMFileReader.setDefaultValidationStringency(SAMFileReader.ValidationStringency.SILENT);
-            final SAMFileReader parser = new SAMFileReader(new File(inputFilename), null);
-
-            progress.start();
-
-            for (final SAMRecord samRecord : parser) {
-                final String readId = samRecord.getReadName();
-                writer.setIdentifier(readId);
-                writer.setSequence(byteToString(samRecord.getReadBases()));
-
-                writer.setQualityScores(FastaToCompactMode.convertQualityScores(qualityEncoding,
-                        byteToString(samRecord.getBaseQualities()),
-                        false));
-                writer.appendEntry();
-                progress.lightUpdate();
+            for (String inputFilename : inputFilenames) {
+                if (processPairs && inputFilename.contains(pairIndicator1)) {
+                    processOneFile(inputFilename, writer);
+                }
             }
         } finally {
             writer.close();
+        }
+    }
+
+    private void processOneFile(String inputFilename, ReadsWriter writer) throws IOException {
+        final ProgressLogger progress = new ProgressLogger(LOG);
+        // the following is required to set validation to SILENT before loading the header (done in the SAMFileReader constructor)
+        SAMFileReader.setDefaultValidationStringency(SAMFileReader.ValidationStringency.SILENT);
+        final SAMFileReader parser = new SAMFileReader(new File(inputFilename), null);
+        final String pairInputFilename;
+        final SAMFileReader pairedParser;
+        final SAMRecordIterator pairedIterator;
+        progress.start();
+        if (processPairs) {
+            pairInputFilename = inputFilename.replace(pairIndicator1, pairIndicator2);
+            LOG.info(String.format("Located paired-end input files (%s,%s)", inputFilename, pairInputFilename));
+            pairedParser = new SAMFileReader(new File(pairInputFilename));
+            pairedIterator = pairedParser.iterator();
+        } else {
+            pairedParser = null;
+            pairedIterator = null;
+            pairInputFilename = null;
+        }
+        int readNumber = 1;
+        for (final SAMRecord samRecord : parser) {
+            final String readId = samRecord.getReadName();
+            writer.setIdentifier(readId);
+            writer.setSequence(byteToString(samRecord.getReadBases()));
+
+            writer.setQualityScores(FastaToCompactMode.convertQualityScores(qualityEncoding,
+                    byteToString(samRecord.getBaseQualities()),
+                    false));
+
+            if (processPairs) {
+                if (pairedIterator != null && !pairedIterator.hasNext()) {
+                    System.err.printf("Paired file %s must have at least as many sequences as the primary input file %s. Aborting. %n",
+                            inputFilename, pairInputFilename);
+                    System.exit(1);
+                } else {
+                    assert pairedIterator != null;
+                    final SAMRecord pairedSamRecord = pairedIterator.next();
+                    if (!pairedSamRecord.getReadName().equals(readId)) {
+
+                        System.err.printf("read identifier must match between paired file %s and primary input file %s." +
+                                " Detected mismatch for read ids =%s %s at sequence number=%d. Aborting. %n",
+                                inputFilename, pairInputFilename, readId, pairedSamRecord.getReadName(), readNumber);
+                        System.exit(1);
+                    }
+
+                    writer.setPairSequence(byteToString(pairedSamRecord.getReadBases()));
+                    writer.setQualityScoresPair(FastaToCompactMode.convertQualityScores(qualityEncoding,
+                            byteToString(samRecord.getBaseQualities()),
+                            false));
+                }
+
+            }
+            readNumber++;
+            if (processAtMost != -1 && readNumber > processAtMost) {
+                break;
+            }
+            writer.appendEntry();
+            progress.lightUpdate();
+            parser.close();
+            if (processPairs && pairedParser != null) {
+                pairedParser.close();
+            }
         }
     }
 
