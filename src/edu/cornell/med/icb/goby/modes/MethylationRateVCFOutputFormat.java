f4c56d083322cc3b6a16cb16a6ae5fe4f595642f
Fabien Campagne <fac2003@campagnelab.org>
2012-03-01 20:37:25 +0000

    Fix tests.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@30975 60cd2ccc-2b05-0410-9566-da273a940bff

--- src/edu/cornell/med/icb/goby/modes/MethylationRateVCFOutputFormat.java
+++ src/edu/cornell/med/icb/goby/modes/MethylationRateVCFOutputFormat.java
@@ -183,8 +183,8 @@ public class MethylationRateVCFOutputFormat extends AbstractOutputFormat impleme
             if (estimateIntraGroupP) {
                 empiricalPValueColumnIndex[comparison.index] = statWriter.defineField("INFO", String.format("empiricalP[%s/%s]", comparison.nameGroup1, comparison.nameGroup2),
                         1, ColumnType.Float, String.format("Empirical P-value of observing as large a difference by chance between group %s and group %s.", comparison.nameGroup1, comparison.nameGroup2));
+                lastOfDMRIndex[comparison.index] = statWriter.defineField("INFO", "DMR", 1, ColumnType.Integer, String.format("Number of significant sites observed in a fixed window of length %d before this significant site (inclusive).", windowLength));
             }
-            lastOfDMRIndex[comparison.index] = statWriter.defineField("INFO", "DMR", 1, ColumnType.Integer, String.format("Number of significant sites observed in a fixed window of length %d before this significant site (inclusive).", windowLength));
 
         }
         methylatedCCountsIndex = new int[groups.length];
