c83237f1c510ca98ffb812f7dcd9d26a391ee6c3
Fabien Campagne <fac2003@campagnelab.org>
2012-04-10 15:57:06 +0000

    remove stray import.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/goby@31421 60cd2ccc-2b05-0410-9566-da273a940bff

--- src/edu/cornell/med/icb/goby/alignments/IterateAlignments.java
+++ src/edu/cornell/med/icb/goby/alignments/IterateAlignments.java
@@ -30,7 +30,6 @@ import it.unimi.dsi.lang.MutableString;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import javax.jnlp.FileSaveService;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
