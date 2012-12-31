927660805ccd62f3432b925118afc3cdd142dde6
Fabien Campagne <fac2003@campagnelab.org>
2012-12-31 11:31:19 -0500

    updated version numbers for python and c++ APIs

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 
 setup(
     name='goby',
-    version='2.1.1',
+    version='2.1.2',
     packages=['goby'],
     author='Campagne Lab',
     author_email='icb@med.cornell.edu',
