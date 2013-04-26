d584c49a1e3f0914c1996828fb1a08b7fd317385
Fabien Campagne <fac2003@campagnelab.org>
2013-04-26 10:47:42 -0400

    Update version numbers, prepare for 2.3 release.

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 
 setup(
     name='goby',
-    version='2.2.1',
+    version='2.3',
     packages=['goby'],
     author='Campagne Lab',
     author_email='icb@med.cornell.edu',
