73c4b06f8db9b439cff8bdff95718694e7f76fd1
Fabien Campagne <fac2003@campagnelab.org>
2014-05-01 17:34:28 -0400

    Increase version number to 2.3.4

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 
 setup(
     name='goby',
-    version='2.3.2',
+    version='2.3.4',
     packages=['goby'],
     author='Campagne Lab',
     author_email='icb@med.cornell.edu',
